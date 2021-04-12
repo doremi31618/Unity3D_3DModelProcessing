d90bb34cb55e8b08c4984ebebae734239c8ec0b8
doremi31618 <doremi31618@gmail.com>
2021-04-12 17:47:33 +0800

    add scripts

---
+++ Assets/Scripts/EditMesh.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+[ExecuteInEditMode]
+public class EditMesh : MonoBehaviour
+{
+    [Range(0.01f, 0.5f)]public float gizmosSize = 0.1f;
+    float _oldGizmosSize;
+    Mesh mesh;
+    Vector3[] vertices;
+    public List<GameObject> handles = new List<GameObject>();
+
+    void OnValueChange(){
+        if (_oldGizmosSize != gizmosSize){
+            foreach(var handle in handles){
+                handle.GetComponent<VertexGizmos>().Initialize(gizmosSize);
+                _oldGizmosSize = gizmosSize;
+            }
+        }
+    }
+
+    void UpdateModel(){
+        Debug.Log("Update model");
+        Vector3[] _vertices = new Vector3[handles.Count];
+        int index = 0;
+        foreach(var handle in handles){
+            _vertices[index ++] = handle.transform.localPosition;
+        }
+        mesh.vertices = _vertices;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+    }
+
+    void OnEnable(){
+        mesh = GetComponent<MeshFilter>().sharedMesh;
+        vertices = mesh.vertices;
+
+        if(handles.Count != 0)return;
+        foreach(var p in vertices){
+            GameObject handle = new GameObject("MeshHandle");
+            handle.transform.parent = this.transform;
+            handle.transform.position = transform.localToWorldMatrix.MultiplyPoint(p);
+            VertexGizmos vertexEditor = handle.AddComponent<VertexGizmos>();
+            vertexEditor.Initialize(gizmosSize);
+            vertexEditor.OnEditPosition += UpdateModel;
+            handles.Add(handle);
+        }
+    }
+
+    void OnDisable(){
+
+        foreach(var handle in handles){
+            DestroyImmediate(handle);
+        }
+        handles.Clear();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OnValueChange();
+    }
+}
+
+#endif
---
+++ Assets/Scripts/VertexGizmos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+[ExecuteInEditMode]
+public class VertexGizmos : MonoBehaviour
+{
+    float size;
+    Vector3 oldPosition;
+    public Action OnEditPosition;
+    public void Initialize(float _size){
+        size = _size;
+        oldPosition = transform.localPosition;
+    }
+
+    /// <summary>
+    /// Callback to draw gizmos that are pickable and always drawn.
+    /// </summary>
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(transform.position, size);
+    }
+
+    void Update(){
+
+        //there's a problem when you scaling the gameObject 
+        //all the gizmos will trigger this at same time, and this would cost 
+        // a lot of efficacy
+        if(oldPosition != transform.localPosition){
+            OnEditPosition();
+            oldPosition = transform.localPosition;
+        }
+    }
+
+}
+
+#endif
