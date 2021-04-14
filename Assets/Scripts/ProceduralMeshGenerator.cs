c6fe21ac8d3e041e19e5076038f8fdcb1a4c6f22
doremi31618 <doremi31618@gmail.com>
2021-04-14 17:08:01 +0800

    Add Mesh Topology features

---
+++ Assets/Scripts/ProceduralMeshGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class ProceduralMeshGenerator : MonoBehaviour
+{
+    public MeshTopology renderMode = MeshTopology.Points;
+    public bool isUseCustomMesh = false;
+    [Header("Custom Mesh")]
+    public Mesh customMesh;
+
+    [Header("Procedural Mesh")]
+    public int vertexNumber;
+    public float minInterval;
+    public Material material;
+
+    Vector3[] ProceduralPoint(int num){
+        Vector3[] points = new Vector3[num];
+        //write generator rule
+
+        return points;
+    }
+    Mesh ProceduralGeneratePointCloud(int _vertexNumber){
+        //first create basic 
+        Mesh mesh = new Mesh();
+        Vector3[] vertices = new Vector3[_vertexNumber];
+        int[] indices = new int[_vertexNumber];
+
+        //determin how to render 3d model
+        //if you choose to render 
+        // 1. point : the "Index buffer" In most of use cases, mesh index buffer should be "identity"
+        mesh.vertices = vertices;
+        mesh.SetIndices(indices, MeshTopology.Points, 0);
+        return mesh;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- README.md
+++ README.md
@@ -20,4 +20,13 @@
     <p> just Disable the script</p>
     <h5>Preview</h5>
     <img src="img/AutoSnap.gif">
+    <h3>Mesh Topology</h3>
+    <h5>Intro</h5>
+    <p> a practice demo that show how to change topology of mesh</p>
+    <h5>How to use</h5>
+    <p> attach SetRenderMode Script to GameObject and remember to enable it</p>
+    <h5>If you don't want to use</h5>
+    <p> just Remove the script</p>
+    <h5>Preview</h5>
+    <img src="img/MeshTopology.png">
 </div>
