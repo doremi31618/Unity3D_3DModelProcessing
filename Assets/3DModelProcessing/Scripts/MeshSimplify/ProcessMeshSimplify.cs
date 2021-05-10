8f73510553bd57da5d28a1ac24db3857056bbe45
doremi31618 <doremi31618@gmail.com>
2021-05-10 16:30:47 +0800

    Add Obj Exporter

---
+++ Assets/3DModelProcessing/Scripts/MeshSimplify/ProcessMeshSimplify.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+/// <summary>
+/// reference : https://github.com/Whinarn/UnityMeshSimplifier
+/// </summary>
+public class ProcessMeshSimplify : MonoBehaviour
+{
+
+    [Range(0,1)]public float quality = 0.05f;
+    void SimplifyMesh(){
+        Mesh sourceMesh = GetComponent<MeshFilter>().mesh;
+        var meshSimplifier = new UnityMeshSimplifier.MeshSimplifier();
+        meshSimplifier.Initialize(sourceMesh);
+        meshSimplifier.SimplifyMesh(quality);
+        GetComponent<MeshFilter>().mesh = meshSimplifier.ToMesh();
+    }
+    // Start is called before the first frame update
+    void Awake(){
+        SimplifyMesh();
+    }
+    void ExportObjFile(){
+        ObjExporter.MeshToFile(GetComponent<MeshFilter>(), Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Simplify");
+    }
+
+    //On Test use
+    void OnGUI(){
+            // Rect btn = new Rect(50, 50, 150, 50);
+            // if (GUI.Button(btn, "Export model"))
+            // {
+            //     ExportObjFile();
+            // }
+    }
+}
